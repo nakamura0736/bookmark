1b4938eb0908a5ef524c2308889e45b9d5af9057
中村圭輝 <mac@nakamuranomac.local>
2020-04-06 23:35:07 +0900

    bookmarks first commit

---
+++ resources/views/bookmarks/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ブックマーク一覧</div>
+
+                <div class="card-body">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>タイトル</th>
+                      </tr>
+                    </thead>
+                    @foreach($bookmarks as $bookmark)
+                      <tr>
+                        <td>{{ $bookmark->id }}</td>
+                        <td>{{ $bookmark->title }}</td>
+                      </tr>
+                    @endforeach
+                  </table>
+                  {{ $bookmarks->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
